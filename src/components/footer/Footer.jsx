f7b72040a7f20cad302953c2df18da16580733bb
bimashazaman <bimashazaman1234@gmail.com>
2022-09-05 14:56:31 +0600

    Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <div className="w-full max-w-[1024px] text-[12px]">
+                <div className="flex w-full justify-between mb-[50px]">
+                    <ul className="">
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Country </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Regions </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Cities </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Destricts </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Airports </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Hotels </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Bimasha </li>
+                    </ul>
+                    {/* end ul by bimasha */}
+
+                    <ul className="">
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Country </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Regions </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Cities </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Destricts </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Airports </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Hotels </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Bimasha </li>
+                    </ul>
+                    {/* end ul by bimasha */}
+
+                    <ul className="">
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Country </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Regions </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Cities </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Destricts </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Airports </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Hotels </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Bimasha </li>
+                    </ul>
+                    {/* end ul by bimasha */}
+
+
+                    <ul className="">
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Country </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Regions </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Cities </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Destricts </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Airports </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Hotels </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Bimasha </li>
+                    </ul>
+                    {/* end ul by bimasha */}
+
+
+                    <ul className="">
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Country </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Regions </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Cities </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Destricts </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Airports </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Hotels </li>
+                        <li className='mb-3 text-blue-900 cursor-pointer'>Bimasha </li>
+                    </ul>
+                    {/* end ul by bimasha */}
+
+
+                </div>
+                {/* end footer List by bimasha */}
+                <div className=''>
+                    <center>
+                        Copyright © 2022 Bijim Reservation
+                    </center>
+                </div>
+            </div>
+            {/* end footer by bimasha */}
+        </>
+    )
+}
+
+export default Footer
+
+
+
+//App Name: Bijim Reservation
+//Author: Bimasha Zaman
+//Contact: developerbimasha@gmail.com
