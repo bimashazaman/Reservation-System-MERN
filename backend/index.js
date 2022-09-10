efdb26fc1d19c3644bb09d496c36b256d2c5364e
bimashazaman <bimashazaman1234@gmail.com>
2022-09-10 12:45:41 +0600

    Backend Started

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.listen(8800, ()=> {
+    console.log("Connected to bacakend");
+})
+
+
+//App Name: Bijim Reservation
+//Author: Bimasha Zaman
+//Contact: developerbimasha@gmail.com
