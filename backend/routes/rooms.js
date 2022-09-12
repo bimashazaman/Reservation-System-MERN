5c1079bbd88b7e56246ba32c65f1e686f84cd324
bimashazaman <bimashazaman1234@gmail.com>
2022-09-12 18:23:16 +0600

    C&D

--- backend/routes/rooms.js
+++ backend/routes/rooms.js
@@ -1,19 +1,31 @@
-
-// import external lib by Bimasha
 import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+  updateRoomAvailability,
+} from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
+const router = express.Router();
+//CREATE
+router.post("/:hotelid", verifyAdmin, createRoom);
 
-const RoomsRouter = express.Router();
+//UPDATE
+router.put("/availability/:id", updateRoomAvailability);
+router.put("/:id", verifyAdmin, updateRoom);
+//DELETE
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+//GET
 
-RoomsRouter.get("/", (req, res) => {
-    res.send('Hi Bimasha Rooms');
-})
+router.get("/:id", getRoom);
+//GET ALL
 
+router.get("/", getRooms);
 
-
-
-export default RoomsRouter
-
+export default router;
 
 
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,22 +1,28 @@
-
-// import external lib by Bimasha
 import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
+
+const router = express.Router();
 
 
-const usersRouter = express.Router();
+//UPDATE
+router.put("/:id", verifyUser, updateUser);
 
-usersRouter.get("/", (req, res) => {
-    res.send('Hi Bimasha users');
-})
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
 
+//GET
+router.get("/:id", verifyUser, getUser);
 
+//GET ALL
+router.get("/", verifyAdmin, getUsers);
 
-
-
-export default usersRouter
-
-
-
+export default router;
 
 //App Name: Bijim Reservation
 //Author: Bimasha Zaman
